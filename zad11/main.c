6afd011abe6dba6ebac14036fbd2c63e885afee1
SiwyInf <115175698+SiwyInf@users.noreply.github.com>
2024-02-22 18:17:07 +0100

    zad11

---
+++ zad11/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main()
+{
+    float a =6;
+    float b =5;
+    float c = sqrt(a * a + b* b);
+    printf(" Przeciwprostakotna to %lf\n", c);
+    return 0;
+}
