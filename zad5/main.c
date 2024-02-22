2ec843872369e754ed0b82bfef743fae66c923a7
SiwyInf <115175698+SiwyInf@users.noreply.github.com>
2024-02-22 17:56:34 +0100

    zad5

---
+++ zad5/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    char a,b;
+    printf("Wpisz dwie litery\n");
+    scanf("%c %c",&a, &b);
+    printf("Litery od tylu %c %c",b,a);
+    return 0;
+}
