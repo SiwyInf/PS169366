54f4270698a7d614b57ef75f31cb56fe5da50e49
SiwyInf <115175698+SiwyInf@users.noreply.github.com>
2024-02-22 17:40:59 +0100

    zad1

---
+++ zad1/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a;
+    printf("Wpisz liczbe calkowita!\n");
+    scanf("%d",&a);
+    printf("Oto twoja liczba %d\n ",a);
+    return 0;
+}
