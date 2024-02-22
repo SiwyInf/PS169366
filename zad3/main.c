3d0c09d9861eea56fbf07aa7faea8f2e4f52bfea
SiwyInf <115175698+SiwyInf@users.noreply.github.com>
2024-02-22 17:47:29 +0100

    zad3

---
+++ zad3/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a;
+    printf("Wpisz swoj rok urodzenia!\n");
+    scanf("%d",&a);
+    printf("Oto rocznik o 1 rok mniejszy %d\n ",a -1);
+    return 0;
+}
