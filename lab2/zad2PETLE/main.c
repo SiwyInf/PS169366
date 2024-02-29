3e7b63cab267f4d9a6ee84062f2de2fe8699f4ec
SiwyInf <115175698+SiwyInf@users.noreply.github.com>
2024-02-29 18:18:47 +0100

    zad2PETLE

---
+++ lab2/zad2PETLE/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a;
+    printf("Wpisz liczbe calkowita\n");
+    scanf("%d",&a);
+    for(int i=0;i<=a;i++){
+        printf("%d\n",i);
+    }
+    return 0;
+}
