d3660c3ab5e04e173e8b4ea963fce5967d7654aa
SiwyInf <115175698+SiwyInf@users.noreply.github.com>
2024-03-14 18:36:29 +0100

    zad7

--- lab4/zad7/main.c
+++ lab4/zad7/main.c
@@ -1,16 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-float *initFlts(){
-float w1 = 0.5;
-float w2 = 1.5;
-float w3 = 2.5;
-float w4 = 3.5;
-return w4;
+float *initFlts() {
+    float *wsk = (float *)malloc(4 * sizeof(float));
+
+    *(wsk + 0) = 0.5;
+    *(wsk + 1) = 1.5;
+    *(wsk + 2) = 2.5;
+    *(wsk + 3) = 3.5;
+
+    return wsk + 3;
 }
 
-int main()
-{
-    printf("%.2f\n", initFlts());
+int main() {
+    float *wynik = initFlts();
+
+    printf("WSKAZNIK: %p\n", *(wynik));
+    printf("1: %.2f\n", *(wynik - 3));
+    printf("2: %.2f\n", *(wynik - 2));
+    printf("3: %.2f\n", *(wynik - 1));
+    printf("4: %.2f\n", *wynik);
+    free(wynik);
     return 0;
 }
