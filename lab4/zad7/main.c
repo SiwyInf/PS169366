43569ae53695b654c080d6f04940dd6b8e55c06c
SiwyInf <115175698+SiwyInf@users.noreply.github.com>
2024-03-14 17:31:41 +0100

    zad7

---
+++ lab4/zad7/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+float initFlts(){
+float w1 = 0.5;
+float w2 = 1.5;
+float w3 = 2.5;
+float w4 = 3.5;
+return w4;
+}
+
+int main()
+{
+    printf("%.2f\n", initFlts());
+    return 0;
+}
